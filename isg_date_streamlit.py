948d8ecfadc142f09ecdbe49da3ec28019384016
demircanmurat <demircanmurat1@gmail.com>
2022-01-17 15:22:22 +0300

    Update isg_date_streamlit.py

--- isg_date_streamlit.py
+++ isg_date_streamlit.py
@@ -212,7 +212,7 @@ with dataset:
 
     #Dataset alınırken hepsi string olarak alındı. Streamlit 1.3 versiyonundan kaynaklı bir numpy kütüphanesinden hata alınıyordu
 
-    df = pd.read_excel(r"data\kaza_date.xlsx").astype(str)
+    df = pd.read_excel("kaza_date.xlsx").astype(str)
     st.write(df.head())
 
     st.markdown("Dataseti aylık olarak yeniden oluşturduk")
